353551885c9880c580cb58d17ac54bf0a7d922dd
Adam <adam.mehdaouijorge@gmail.com>
2024-03-11 19:00:49 +0100

    doc + fix vercel deploy

---
+++ src/components/DynamicDeconnectionButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { deconnection } from "@/utils/connection";
+import { toast } from "sonner";
+
+function handleDeconnection() {
+  deconnection();
+  toast.info("Vous êtes déconnecté");
+}
+
+export default function DeconnectionButton() {
+  return (
+    <button
+      onClick={handleDeconnection}
+      className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none focus:shadow-outline-red active:bg-red-800 mt-4 ml-4 w-96"
+    >
+      Se déconnecter
+    </button>
+  );
+}
