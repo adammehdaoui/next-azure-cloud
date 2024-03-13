f9be681e9e1f2a0c6150a8017f73515b9bf96481
Adam <adam.mehdaouijorge@gmail.com>
2024-03-13 19:05:59 +0100

    feat: launch ubuntu & centos & windows distrib

---
+++ src/config/vms.ts
@@ -0,0 +1,23 @@
+export const vms = [
+  {
+    name: "CentOS",
+    image: "centOs.svg",
+    publisher: "OpenLogic",
+    offer: "CentOS",
+    sku: "8_5-gen2",
+  },
+  {
+    name: "Windows",
+    image: "windows.svg",
+    publisher: "MicrosoftWindowsServer",
+    offer: "WindowsServer",
+    sku: "2019-datacenter-core",
+  },
+  {
+    name: "Ubuntu",
+    image: "ubuntu.svg",
+    publisher: "Canonical",
+    offer: "UbuntuServer",
+    sku: "14.04.3-LTS",
+  },
+];
